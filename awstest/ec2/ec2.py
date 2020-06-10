94b545c99d5804d99a58614ef4c23afdf28274ed
tuimac <tuimac.devadm01@gmail.com>
2020-06-10 05:05:16 +0000

    add url query function

--- awstest/ec2/ec2.py
+++ awstest/ec2/ec2.py
@@ -1,19 +1,32 @@
 import urllib.request
 import traceback
+import json
 
 class EC2:
     def __init__(self):
         self.baseurl = "http://169.254.169.254/latest/"
         self.path = {
-            "InstanceType": "meta-data/instance-type"
+            "InstanceType": "meta-data/instance-type",
+            "InstanceIDDocument": "dynamic/instance-identity/document"
         }
+        self.response = dict()
 
-    def query(self, target) -> str:
+    def __queryAll(self):
+        pass
+
+    def query(self, target="all"):
+        if target == "all": return __queryAll()
         try:
             url = self.baseurl + self.path[target]
             response = urllib.request.Request(url)
             with urllib.request.urlopen(response) as data:
-                return data.read().decode("ascii")
+                body = data.read().decode("ascii")
+                try:
+                    self.response[target] = json.loads(body)
+                    return self.response
+                except json.decoder.JSONDecodeError as e:
+                    self.response[target] = body
+                    return self.response
         except KeyError as e:
             return "Target Error"
         except urllib.error.HTTPError as e:
--- awstest/ec2/tests.py
+++ awstest/ec2/tests.py
@@ -4,4 +4,5 @@ from ec2 import EC2
 
 if __name__ == "__main__":
     ec2 = EC2()
-    ec2.query("InstanceType")
+    print(ec2.query("InstanceType"))
+    print(ec2.query("InstanceIDDocument"))
