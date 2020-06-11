0d92a31138987f7cba73652f76c7b374760751a6
tuimac <tuimac.devadm01@gmail.com>
2020-06-11 07:10:23 +0000

    fixe

--- awstest/awstest/settings.py
+++ awstest/awstest/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'ec2'
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,30 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+
+# Logging configuration
+# https://docs.djangoproject.com/en/3.0/topics/logging/
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'file': {
+            'format': '%s'
+        }
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/root/awstest/awstest/awstest/django.log'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': DEBUG,
+            'propagate': True,
+        },
+    },
+}
--- awstest/ec2/views.py
+++ awstest/ec2/views.py
@@ -1,8 +1,16 @@
 from rest_framework import views, status
 from rest_framework.response import Response
 from ec2 import EC2
+import logging
+import traceback
+
+logger = logging.getLogger(__name__)
 
 class EC2APIViews(views.APIView):
     def get(self, request, *args, **kwargs):
-        response = ec2.query()
-        return Response(response)
+        try:
+            response = ec2.query()
+            return Response(response)
+        except:
+            message = "".join(traceback.format_list())
+            logger.debug(message)
