99e65bd80d14df378e4dc7e809dd41d8add5d635
tuimac <tuimac.devadm01@gmail.com>
2020-06-12 08:32:51 +0000

    fixed

--- awstest/ec2/views.py
+++ awstest/ec2/views.py
@@ -1,15 +1,8 @@
 from rest_framework import views, status
 from rest_framework.response import Response
 from .ec2 import EC2
-import logging
-import traceback
-
-logger = logging.getLogger(__name__)
 
 class EC2APIViews(views.APIView):
     def get(self, request, *args, **kwargs):
-        try:
-            response = ec2.query()
-            return Response(response)
-        except:
-            logger.error("".join(traceback.format_list()))
+        response = ec2.query()
+        return Response(response)
