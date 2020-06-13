cbd5ac6d13e039d4dc7b447b13cca99683f983c6
tuimac <tuimac.devadm01@gmail.com>
2020-06-13 04:19:43 +0000

    add cors setting

--- awstest/awstest/settings.py
+++ awstest/awstest/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'ec2',
-    'container'
+    'container',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'awstest.urls'
