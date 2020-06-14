6f65e4b794c476343dc817f44d016a40fa1060a5
tuimac <tuimac.devadm01@gmail.com>
2020-06-14 08:48:30 +0000

    change awstest to httptracker

--- httptracker/manage.py
+++ httptracker/manage.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'awstest.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'httptracker.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
