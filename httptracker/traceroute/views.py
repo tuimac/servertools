71223ce3d292f01c56ef16bb2acc862aee20732d
tuimac <tuimac.devadm01@gmail.com>
2020-06-20 19:19:42 +0900

    delete debugger line

--- httptracker/traceroute/views.py
+++ httptracker/traceroute/views.py
@@ -14,7 +14,6 @@ class TracerouteAPIViews(views.APIView):
     def get(self, request, *args, **kwargs):
         try:
             traceroute = Traceroute(request.META.get("REMOTE_ADDR"))
-            logger.error(traceroute)
             response = traceroute.traceroute()
             response["Url"] = "traceroute"
             return Response(response)
