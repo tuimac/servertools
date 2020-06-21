96bfcbd4987019fac9a1d4581b2a0ea3195c084a
tuimac <tuimac.devadm01@gmail.com>
2020-06-21 16:48:44 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import os
+import shutil
+
+# Setuptools
+setup(
+    name="httptracker",
+    version="1.0.0",
+    url="https://github.com/tuimac/httptracker",
+    author="tuimac",
+    author_email="tuimac.devadm01@gmail.com",
+    license="LICENSE.md",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.4",
+       	"Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: System :: Installation/Setup",
+        "Topic :: System :: Software Distribution"
+    ],
+    description="Tools of tracing http requests.",
+    python_requires=">=3.4.0",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "django>=3.0.0",
+        "djangorestframework",
+        "django-filter",
+        "django-cors-headers",
+        "setuptools"
+    ]
+)
