e2df9586b9b9f335e336c55b6d92beb08748663f
tuimac <tuimac.devadm01@gmail.com>
2020-06-23 10:34:01 +0900

    create new

--- httptracker/__main__.py
+++ httptracker/__main__.py
@@ -2,6 +2,9 @@
 
 import subprocess
 import sys
+import traceback
+
+PORT = '8000'
 
 def usage():
     print('usage: httptracker [help | start | restart | stop]\n')
@@ -10,16 +13,25 @@ def usage():
     print('stop        Stop httptracker process\n')
 
 def main():
-    if len(sys.argv) != 1:
-        usage()
-        sys.exit(1)
-    arg = sys.argv[1]
-    if arg == 'start':
-    
-    elif arg == 'restart':
-    elif arg == 'stop':
-    else:
-
+    try:
+        if len(sys.argv) != 2:
+            usage()
+            sys.exit(1)
+        arg = sys.argv[1]
+        if arg == 'start':
+            subprocess.run(['python3', 'manage.py', 'runserver', '0.0.0.0:' + PORT])
+        elif arg == 'restart':
+            pass
+        elif arg == 'stop':
+            pass
+        elif arg == 'help':
+            usage()
+        else:
+            usage()
+    except SystemExit:
+        pass
+    except:
+        traceback.print_exc()
 
 if __name__ == '__main__':
     main()
