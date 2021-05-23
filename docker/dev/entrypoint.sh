24f34ae6a2c8d2c1d328b604bfb030c877f8debf
Ubuntu <ubuntu@docker.tuimac.private>
2021-05-23 07:34:39 +0000

    save

--- docker/dev/entrypoint.sh
+++ docker/dev/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-python3 /root/servertools/src/backend/manage.py runserver 0.0.0.0:8000 &
-
 cd /root/servertools/src/frontend
 npm start &
 
 cd /root
 redis-server &
+
+python3 /root/servertools/src/backend/manage.py runserver 0.0.0.0:8000 &
 
 /usr/sbin/nginx -g 'daemon off;' -c /etc/nginx/nginx.conf
