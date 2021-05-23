fd22a0fbf84eeb343311d135f98dfc76c55a46aa
tuimac <tuimac.devadm01@gmail.com>
2021-05-23 16:07:14 +0000

    save

--- src/frontend/src/components/Runcommand.js
+++ src/frontend/src/components/Runcommand.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Terminal } from 'xterm';
 import {
   API_URL
 } from '../environment';
@@ -13,6 +14,7 @@ class Runcommand extends React.Component {
       command: '',
     };
     this.handleChange = this.handleChange.bind(this);
+    console.log(window.location.hostname);
   }
 
   handleChange({ target }) {
@@ -21,12 +23,49 @@ class Runcommand extends React.Component {
     });
   }
 
+  componentDidMount() {
+    var term = new Terminal({
+      cursorBlink: true
+    });
+    term.open(this.terminal);
+    const url = 'ws://' + window.location.hostname + API_URL + '/runcommand'
+    const socket = new WebSocket(url);
+    term.write('$ ');
+    let current_line = "";
+    let cursor = 0;
+  	term.onKey((data) => {
+      switch(data.domEvent.key) {
+        case "Enter":
+          term.write("\r\n");
+          if(current_line) {
+            socket.send(JSON.stringify({
+              command: current_line
+            }));
+          } else {
+            term.write("\r\n");
+            term.write("$ ");
+          }
+          cursor = 0;
+          current_line = "";
+          break;
+        default:
+          if (cursor < 120) {
+            cursor += 1;
+            current_line += data.key;
+            term.write(data.key);
+          }
+          break;
+      }
+    });
+  }
+
   render() {
     return (
       <>
         <div>
-          Runcommand
-          { API_URL }
+          <div style={{padding:'10px'}}>
+            <div ref={ref=>this.terminal = ref}></div>
+          </div>
         </div>
       </>
     );
