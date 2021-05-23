d51423bd18f3355b3ed7a1e8b47c0590e19cdc17
Ubuntu <ubuntu@docker.tuimac.private>
2021-05-23 07:20:59 +0000

    add redis

--- docker/dev/entrypoint.sh
+++ docker/dev/entrypoint.sh
@@ -5,4 +5,7 @@ python3 /root/servertools/src/backend/manage.py runserver 0.0.0.0:8000 &
 cd /root/servertools/src/frontend
 npm start &
 
+cd /root
+redis-server &
+
 /usr/sbin/nginx -g 'daemon off;' -c /etc/nginx/nginx.conf
