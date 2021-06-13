09439dc42842984463aa2b93e56ab90da174b672
tuimac <tuimac.devadm01@gmail.com>
2021-06-13 04:00:54 +0000

    save

--- src/backend/runshell/tests.py
+++ src/backend/runshell/tests.py
@@ -1,9 +1,35 @@
 #!/usr/bin/env python3
 
-from container import Container
+import os
+import subprocess
+import socket
+import pty
+import traceback
+from threading import Thread
+import time
+
+def stderr(process):
+    while True:
+        print(p.stderr.readline())
+
+def stdout(process):
+    while True:
+        print(p.stdout.readline())
 
 if __name__ == "__main__":
-    container = Container()
-    #print(container.query("InstanceType"))
-    #print(container.query("InstanceIDDocument"))
-    print(container.query())
+    p = subprocess.Popen(
+        ['/bin/bash'],
+        shell = False,
+        stdin = subprocess.PIPE,
+        stdout = subprocess.PIPE,
+        stderr = subprocess.PIPE,
+    )
+    out = Thread(target=stdout, args=(p, ))
+    out.setDaemon(True)
+    out.start
+    err = Thread(target=stderr, args=(p, ))
+    err.setDaemon(True)
+    err.start
+    time.sleep(2)
+    p.stdin.write('cd /\n'.encode())
+    p.stdin.write('ls\n'.encode())
