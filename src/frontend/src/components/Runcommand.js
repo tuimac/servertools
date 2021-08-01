9a4f5814d3fd6f6708d967c52da1e456d6c5b46c
tuimac <tuimac.devadm01@gmail.com>
2021-08-01 09:33:54 +0000

    save

--- src/frontend/src/components/Runcommand.js
+++ src/frontend/src/components/Runcommand.js
@@ -1,107 +1,23 @@
 import React from 'react';
-import { Terminal } from 'xterm';
-import 'xterm/css/xterm.css';
 import { API_URL } from '../environment';
-import { Card } from 'react-bootstrap';
+import { Card, Nav } from 'react-bootstrap';
+import Console from './Console';
 
 class Runcommand extends React.Component {
   
-  componentDidMount() {
-    const url = 'ws://' + window.location.hostname + API_URL + '/runcommand'
-    const socket = new WebSocket(url);
-    let currentInput = '';
-    let inputHistory = [];
-    let cursor = 0;
-    let historyIndex = 0;
-
-    var term = new Terminal({
-      background: '#202B33',
-      foreground: '#F5F8FA',
-      cursorStyle: 'block',
-      fontSize: 16,
-      cursorBlink: true
-    });
-    
-    term.open(this.terminal);
-    term.write('$ ');
-    socket.addEventListener('message', (response) => {
-      console.log(response);
-      response = JSON.parse(response.data).result.replaceAll('\n', '\r\n')
-      term.write(response + '\r\n');
-      term.write('$ ');
-    });
-
-  	term.onKey((data) => {
-      console.log(data.domEvent.key); 
-      switch(data.domEvent.key) {
-        case 'Enter':
-          term.write('\r\n');
-          if(currentInput) {
-            socket.send(JSON.stringify({
-              command: currentInput
-            }));
-          } else {
-            term.write('\r\n');
-            term.write('$ ');
-          }
-          historyIndex++;
-          inputHistory.push(currentInput);
-          currentInput = '';
-          break;
-        case 'Backspace':
-          if(currentInput.length > 0) {
-            term.write('\b \b');
-          }
-          currentInput = currentInput.substring(0, currentInput.length - 1);
-          break;
-        case 'ArrowUp':
-          try {
-            if(historyIndex > 0 && historyIndex <= inputHistory.length) {
-              historyIndex--;
-              for(let i = 0; i < currentInput.length; i++){
-                term.write('\b \b');
-              }
-              currentInput = inputHistory[historyIndex];
-              term.write(currentInput);
-            }
-          } catch(e) {
-            currentInput = inputHistory[historyIndex];
-            term.write(currentInput);
-          }
-          break;
-        case 'ArrowDown':
-          try {
-            if(historyIndex >= 0 && historyIndex < inputHistory.length) {
-              historyIndex++;
-              for(let i = 0; i < currentInput.length; i++){
-                term.write('\b \b');
-              }
-              currentInput = inputHistory[historyIndex];
-              term.write(currentInput);
-            }
-          } catch(e) {
-            currentInput = inputHistory[historyIndex];
-          }
-          break;
-        default:
-          if (cursor < 120) {
-            cursor += 1;
-            currentInput += data.key;
-            term.write(data.key);
-          }
-          break;
-      }
-    });
-  }
-
   render() {
     return (
       <>
-        <Card className="text-center">
-          <Card.Body>
-            <div ref={ id => this.terminal = id }></div>
-          </Card.Body>
-        </Card>
+        <Nav variant="tabs">
+          <Nav.Item>
+            <Nav.Link>Console 1</Nav.Link>
+            <Card className="text-center">
+              <Card.Body>
+                <Console />
+              </Card.Body>
+            </Card>
+          </Nav.Item>
+        </Nav>
       </>
     );
   };
